e74f1c098840c1f6234b9f05ff99a05b479a9f3c
Erica Forget <erica.forget@gmail.com>
2017-02-10 17:01:03 -0600

    thumbnail working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,10 @@
 get '/' do
+  @type = params[:origami_type]
+  if request.xhr?
+    "#{@type}"
+  else
   erb :index
+  end
 end
 
 # get '/cranes/:id' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
+
+  var currentOrigamiType = 'origami-type';
+
   $(".thumbnail").on('click', function(e){
     e.preventDefault();
-    console.log(this);
-
+    // console.log($(this).closest('a').attr('name'));
+    var choice = $(this).closest('a').attr('name');
+    var num_of_img = $(this).closest('a').attr('number');
+    $('.next').data('num-of-img', num_of_img);
+    // console.log($('.next').data('origami_type'));
+    $.ajax({
+      url: '/',
+      method: 'get',
+      data: { origami_type: choice }
+    })
+    .done(function(response){
+    $('.next').data(currentOrigamiType, response);
+    // console.log($('.next').data(currentOrigamiType));
+    })
   })
 
   $(".next").on("click", function(e){
     e.preventDefault();
-    var $this = $(this)
-    var step = parseInt($this.data('step-id'))
-    if (step < 29){
+    var $this = $(this);
+    var step = parseInt($this.data('step-id'));
+    // debugger;
+    var type = $this.data(currentOrigamiType);
+    var total_num_of_img = parseInt($this.data('num-of-img'));
+    if (step < total_num_of_img){
       step++;
-      $this.find("img").replaceWith('<img class="main_image" src="/images/crane' + step + '.jpg">');
+      $this.find("img").replaceWith('<img class="main_image" src="/images/' + type + step + '.jpg">');
       $this.data('step-id', step);
     }
     else {
@@ -23,11 +41,13 @@ $(document).ready(function() {
   $(".back").on("click", function(e){
     e.preventDefault();
     var step = parseInt($('.next').data('step-id'));
-    console.log(step);
-    if (step >= 2 && step <= 29) {
+    var type = $('.next').data(currentOrigamiType)
+    var total_num_of_img = parseInt($('.next').data('num-of-img'));
+    // console.log(step);
+    if (step >= 2 && step <= total_num_of_img) {
       step--;
-      console.log(step)
-      $('.next').find("img").replaceWith('<img class="main_image" src="/images/crane' + step + '.jpg">');
+      // console.log(step)
+      $('.next').find("img").replaceWith('<img class="main_image" src="/images/'+ type + step + '.jpg">');
       $('.next').data('step-id', step);
     }
     else if (step == 1) {
@@ -36,8 +56,8 @@ $(document).ready(function() {
       $('.next').data('step-id', step);
     }
     else {
-       $('.next').find("img").replaceWith('<img class="main_image" src="/images/crane29.jpg">')
-       $('.next').data('step-id', 29)
+       $('.next').find("img").replaceWith('<img class="main_image" src="/images/' + type + total_num_of_img + '.jpg">');
+       $('.next').data('step-id', total_num_of_img);
     }
   });
 

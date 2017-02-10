3fda5ccd8c4317d72d8f1170e332df949ec21baa
Erica Forget <erica.forget@gmail.com>
2017-02-10 11:55:36 -0600

    got thumbnail the right size, still need to center

--- public/js/application.js
+++ public/js/application.js
@@ -1,42 +1,51 @@
 $(document).ready(function() {
-  $(".next").on("click", function(e){
+  $(".thumbnail").on('click', function(e){
     e.preventDefault();
     console.log(this);
+
+  })
+
+  $(".next").on("click", function(e){
+    e.preventDefault();
     var $this = $(this)
     var step = parseInt($this.data('step-id'))
     if (step < 29){
-    step++;
-    console.log(step)
-    $this.find("img").replaceWith('<img src="/images/crane' + step + '.jpg">');
-    $this.data('step-id', step);
+      step++;
+      $this.find("img").replaceWith('<img class="main_image" src="/images/crane' + step + '.jpg">');
+      $this.data('step-id', step);
     }
     else {
-      $this.find("img").replaceWith('<img src="/images/start.jpg">');
+      $this.find("img").replaceWith('<img class="main_image" src="/images/start.jpg">');
       $this.data('step-id', 0);
     }
-  })
+  });
 
   $(".back").on("click", function(e){
     e.preventDefault();
-    console.log(this);
-    var $this = $(this);
-    var step = parseInt($this.siblings('a').data('step-id'));
+    var step = parseInt($('.next').data('step-id'));
     console.log(step);
     if (step >= 2 && step <= 29) {
-    step--;
-    console.log(step)
-    $this.siblings('a').find("img").replaceWith('<img src="/images/crane' + step + '.jpg">');
-    $this.siblings('a').data('step-id', step);
+      step--;
+      console.log(step)
+      $('.next').find("img").replaceWith('<img class="main_image" src="/images/crane' + step + '.jpg">');
+      $('.next').data('step-id', step);
     }
     else if (step == 1) {
       step = 0;
-      $this.siblings('a').find("img").replaceWith('<img src="/images/start.jpg">');
-      $this.siblings('a').data('step-id', step);
-    } else {
-       $this.siblings('a').find("img").replaceWith('<img src="/images/crane29.jpg">')
-       $this.siblings('a').data('step-id', 29)
+      $('.next').find("img").replaceWith('<img class="main_image" src="/images/start.jpg">');
+      $('.next').data('step-id', step);
     }
-  })
+    else {
+       $('.next').find("img").replaceWith('<img class="main_image" src="/images/crane29.jpg">')
+       $('.next').data('step-id', 29)
+    }
+  });
 
+  $(".fa-home").on("click", function(e){
+    e.preventDefault();
+    var step = parseInt($('.next').data('step-id'));
+    $('.next').find("img").replaceWith('<img class="main_image" src="/images/start.jpg">');
+    $('.next').data('step-id', 0);
+  });
 
 });
